68fc8cc8ec73d585a3925aae7d8114f869ca4a83
VictoriaHilko <47423052+VictoriaHilko@users.noreply.github.com>
2023-10-03 00:12:37 +0300

    Update Searchbar.jsx

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -16,7 +16,7 @@ export class Searchbar extends Component {
         const { onSubmit } = this.props;
 
         if (query.trim() === '') {
-            Notify.failure('Please enter a search value');
+            Notify.info('Please enter some valu to search');
             return;
         }
 
